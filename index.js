529c9c84371eb306974b0e6eda0802899430b088
sneha mestry <sneha@gmail.com>
2021-10-03 15:21:59 +0530

    promises and asinc await

--- index.js
+++ index.js
@@ -15,16 +15,68 @@ function randomText(length) {
 
 let text= console.log(randomText(5));
 
+function writeFilePromise(fileLocation, result) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(fileLocation, result, (err) => {
+        if (err) {
+          reject('not able to write to the file')
+        }
+        resolve()
+      })
+    })
+  }
+
+
+  // 3. async await
+
+async function getDogPic() {
+    try {
+      const res = await superagent.get(`https://robohash.org/${text}`)
+    
+      console.log('robot image is ', res.request.url)
+      await writeFilePromise('./robotImage.txt', res.request.url)
+      console.log('sucessfully written the file')
+    } catch (err) {
+      throw err
+    }
+    
+  }
+ (async () => {
+    try {
+      await getDogPic()
+      console.log('end')
+    } catch (err) {
+      console.log(' end due to error')
+    }
+  })()
+
+// 2. promise
+
+  /* return superagent.get(`https://robohash.org/${text}`)
+
+   .then((res) => {
+     console.log('robot image is ', res.request.url)
+     return writeFilePromise('./robotImage.txt', res.request.url)
+   })
+   .then(() => {
+     console.log('sucessfully written the file')
+   })
+   .catch((err) => {
+     console.log(err)
+   })
+*/
+
 //1. callback hell
 
-
-       superagent
-         .get(`https://robohash.org/${text}`)
+ /*  superagent
+ .get(`https://robohash.org/${text}`)
          .then((res) => {
            console.log('robot image is ', res.request.url)
            fs.writeFile('./robotImage.txt', res.request.url, () => {
            console.log('sucessfully written the file')
-          })
-         })
+     })
+ })
+
+ */
      
    
